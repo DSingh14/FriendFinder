a2ab068c3ba33537716be9b2c7759ada7ef7da8d
Deepali Singh <14singh.deepali@gmail.com>
2019-07-17 23:25:51 -0500

    connection

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friendsArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Jack",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 2, 4, 4, 5, 2, 2, 5, 4, 2]
+
+    },
+    {
+        "name": "Jill",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 3, 3, 5, 1, 2, 5, 3, 1]
+    },
+    {
+        "name": "Mary",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 1, 4, 4, 5, 1, 2, 1, 4, 1]
+
+    },
+
+    {
+        "name": "JOlly",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 2, 2, 5, 1, 2, 5, 4, 1]
+
+
+    }]
+
+module.exports = friendsArray;
