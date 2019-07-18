97d5e2a540e7e4b3caa1dab92a5c7a79ad81722d
Deepali Singh <14singh.deepali@gmail.com>
2019-07-17 23:45:43 -0500

    correction

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,13 @@ var path = require("path");
 // Routing
 module.exports = function (app) {
     //  HTML get request for navigation from home page to survey page
+    //  user enters survey via HTMl file
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "..public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
     // if survey result is shown, back to home page
-    app.get("*", function (req, res) {
+    app.use("#", function (req, res) {
         res.sendFile(path.join(__dirname, "..public/home.html"));
     });
 };
+
