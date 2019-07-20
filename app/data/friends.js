dfd018a7855374b5586eca2ef94ed6872d1eb92e
Deepali Singh <14singh.deepali@gmail.com>
2019-07-19 20:36:44 -0500

    home button

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,30 +1,30 @@
 var friends = [
     {
         "name": "Ahmed",
-        "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+        "photo": "http://images6.fanpop.com/image/photos/37000000/Nova-Scotia-Duck-Tolling-Retriever-dogs-37018367-448-540.jpg",
         "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         "name": "Jack",
-        "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+        "photo": "http://images6.fanpop.com/image/photos/34700000/cute-fluffy-dog-dogs-34707056-485-485.jpg",
         "scores": [5, 2, 4, 4, 5, 2, 2, 5, 4, 2]
 
     },
     {
         "name": "Jill",
-        "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+        "photo": "http://images4.fanpop.com/image/photos/16700000/Dogs-dogs-16762076-1024-768.jpg",
         "scores": [5, 1, 3, 3, 5, 1, 2, 5, 3, 1]
     },
     {
         "name": "Mary",
-        "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+        "photo": "http://images6.fanpop.com/image/photos/33600000/Be-Our-Valentines-dogs-33628006-1600-1200.jpg",
         "scores": [1, 1, 4, 4, 5, 1, 2, 1, 4, 1]
 
     },
 
     {
         "name": "JOlly",
-        "photo": "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+        "photo": "http://images2.fanpop.com/images/photos/7000000/Beagle-Wallpaper-dogs-7013946-1024-768.jpg",
         "scores": [5, 1, 2, 2, 5, 1, 2, 5, 4, 1]
 
 
